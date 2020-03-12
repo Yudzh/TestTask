e2fa138f0ce964965238f268f600631ad5d3915a
Yudzh <egextra98@gmail.com>
2020-03-12 14:48:10 +0700

    Made the maven profile

--- ApiTests/test/java/steps/TestRunner.java
+++ ApiTests/test/java/steps/TestRunner.java
@@ -6,7 +6,6 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-//        tags = {"@JSON"},
         features = "ApiTests/test/java/features",
         glue = "steps"
 )
